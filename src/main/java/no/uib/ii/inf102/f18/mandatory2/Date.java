78a3a788841b77b286d95ec52c7ac28c249dbdec
AndreasOsDev <30537858+AndreasOsDev@users.noreply.github.com>
2018-11-05 23:56:53 +0100

    Revert "Inf102 mandatory2"
    This reverts commit 50e7aebeebd9be8d82a46143ebe918e47954ec8b.

--- src/main/java/no/uib/ii/inf102/f18/mandatory2/Date.java
+++ src/main/java/no/uib/ii/inf102/f18/mandatory2/Date.java
@@ -1,7 +1,5 @@
 package no.uib.ii.inf102.f18.mandatory2;
 
-import java.util.Objects;
-
 /**
  * @author You
  */
@@ -14,20 +12,5 @@ public final class Date {
         this.month = month;
         this.year = year;
         this.day = day;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Date date = (Date) o;
-        return year == date.year &&
-                day == date.day &&
-                month == date.month;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(month, year, day);
     }
 }
