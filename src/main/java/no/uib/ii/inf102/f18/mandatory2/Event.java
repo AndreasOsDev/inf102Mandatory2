da02432ff7560965486c631f65d96a7fa21cdb3d
AndreasOsDev <30537858+AndreasOsDev@users.noreply.github.com>
2018-11-05 23:58:32 +0100

    Revert "Revert "Revert "Inf102 mandatory2"""
    This reverts commit 389342eb8369724de2c4e648b47d165964d9779d.

--- src/main/java/no/uib/ii/inf102/f18/mandatory2/Event.java
+++ src/main/java/no/uib/ii/inf102/f18/mandatory2/Event.java
@@ -1,7 +1,5 @@
 package no.uib.ii.inf102.f18.mandatory2;
 
-import java.util.Objects;
-
 /**
  * @author You
  */
@@ -13,20 +11,5 @@ public class Event {
     public Event(Date date, String title) {
         this.date = date;
         this.title = title;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Event event = (Event) o;
-        return Objects.equals(date, event.date) &&
-                Objects.equals(title, event.title) &&
-                Objects.equals(description, event.description);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(date, title, description);
     }
 }
